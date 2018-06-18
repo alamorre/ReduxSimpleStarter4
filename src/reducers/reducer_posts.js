33399a6edbaf1fe96fa336f70c0772d9c4ccdb3f
Adam La Morre <adam.lamorre@mail.mcgill.ca>
2018-06-18 14:59:20 -0400

    setting up actions, axios request, and reducer

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,13 @@
+import FETCH_POSTS from '../actions';
+import _ from 'lodash';
+
+                              //vvv Were converting the list to an object!
+export default function(state = {}, action){
+  switch (action.type) {
+    case FETCH_POSTS:
+      return _.mapKeys(action.payload.data, 'id');
+    default:
+      return state;
+
+  }
+}
