763eb2ae5cd9e00746b070f2d715848dedfc7644
Adam La Morre <adam.lamorre@mail.mcgill.ca>
2018-06-19 08:53:34 -0400

    added navigation

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
+import { Link } from 'react-router-dom';
 
 class PostsShow extends Component{
+  componentDidMount(){
+    const { id } =this.props.match.params; // id from url
+    this.props.fetchPost(id);
+  }
 
   render(){
+    const { post } = this.props;
+
+    if(!post){
+      return <div>Loading...</div>;
+    }
+
     return(
       <div>
-        Posts show!
+        <Link to="/">Back to Home</Link>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <h3>{post.content}</h3>
       </div>
     );
   }
 }
+                                  // vvv props object that is headed to this component
+function mapStateToProps({ posts }, ownProps){
+  return { post: posts[ownProps.match.params.id] };
+}
 
-export default PostsShow;
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
