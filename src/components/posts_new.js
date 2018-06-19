00b05733c9ca330acd7b0319e4ad11bff853d4ed
Adam La Morre <adam.lamorre@mail.mcgill.ca>
2018-06-19 06:35:41 -0400

    form fields appear on screen with redux-form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component{
+  renderField(field){ // field contains some event-handlers
+    return(
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
   render(){
     return (
-      <div>
-        PostsNew!
-      </div>
+      <form>
+        <Field
+          label = "Title" // instead of passing a param
+          name="title" // the key for JSON to be POSTed
+          component={this.renderField} // Needed JSX
+        />
+        <Field
+          label = "Tags" // key and val both can be anything
+          name="tags"
+          component={this.renderField}
+        />
+        <Field
+          label = "Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm' // This has to be a unique name and name does not matter
+})(PostsNew);
