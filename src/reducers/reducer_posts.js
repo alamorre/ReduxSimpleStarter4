f6db765f2026362625ef5df68968fa1a11ca2233
Adam La Morre <adam.lamorre@mail.mcgill.ca>
2018-06-19 08:27:05 -0400

    fetch post action created and wired to reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,15 @@
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 import _ from 'lodash';
 
                               //vvv Were converting the list to an object!
 export default function(state = {}, action){
   switch (action.type) {
+    case FETCH_POST:
+      // const post = action.patload.data;
+      // const newState = { ...state,  };
+      // newState[post.id] = post;
+      // return newState;
+      return { ...state, [action.payload.data.id]: action.payload.data };
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
     default:
